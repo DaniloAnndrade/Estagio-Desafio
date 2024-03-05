25d7feab0ccf39f9b9b83de7c225df47dc83f931
Danilo Andrade <danilo16pereira@gmail.com>
2024-03-05 13:21:53 -0300

    Create Desafio.py

---
+++ Desafio.py
@@ -0,0 +1,16 @@
+# 1) Observe o trecho de código abaixo:
+
+indice = 13
+soma = 0
+k = 0
+
+while(k < indice):
+    k = k +1
+    soma = soma + k
+
+print(soma) # O a Soma e == 91
+
+
+# Ao final do processamento, qual será o valor da variável SOMA?
+
+
